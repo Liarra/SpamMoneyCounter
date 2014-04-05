bad97b4b995b73c0516031d7f3c070a19b800ec5
Nina Buchina <m.e.tigra@gmail.com>
2014-04-06 00:23:20 +0200

    Replace .pyw by .py and added dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
+
 setup(
     name='spam_money_counter',
     version='1.0',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
-     entry_points={
+    entry_points={
         'console_scripts':
             ['spammoneycounter = smc.main']
-        }
+     },
+    requires=['wx'],
+    
 )
